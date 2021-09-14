eb3f8bd3d83da1bb9ff5c33e50e73f60eaaf0c68
Juan Diego <caricato665@gmail.com>
2021-09-13 20:19:23 -0500

    feat: adding services for supplies

---
+++ services/insumos/supervisor-service.js
@@ -0,0 +1,29 @@
+import stampit from 'stampit';
+import ServiceStamp from '~/services/service';
+
+export const SupplyServiceStamp =stampit.methods({
+  getPaginatedSupplies({companyId}){
+    return this.fetchApi.get(`supplies/${companyId}`);
+  },
+  getAllSupplies({companyId}){
+    return this.fetchApi.get(`supplies/aux/${companyId}`);
+  },
+  deleteSupply({supplyId}){
+    return this.fetchApi.delete(`supplies/${supplyId}`);
+  },
+  getSupplyMetricTypes(){
+    return this.fetchApi.get('supplies/metric_types');
+  },
+  editSupply({supply}){
+    return this.fetchApi.put(`supplies/${supply.supplyId}`, supply);
+  },
+  registerSupply({supply}){
+    return this.fetchApi.post(`supplies`, supply);
+  },
+  exportSupplies({communityId, search}){
+    return this.fetchApi.get(`supplies/reports/${communityId}`, search);
+  },
+});
+
+const SupplyService = stampit.compose(ServiceStamp, SupplyServiceStamp);
+export default SupplyService;
---
+++ store/insumos/supply.js
@@ -0,0 +1,114 @@
+import SupplyService from "~/services/insumos/supervisor-service";
+import Vue from 'vue';
+
+export const state = () => ({
+  loading: true,
+  error: null,
+  message: null,
+  page:{},
+  allSupplies:[],
+  supplies: [],
+  metricTypes:[],
+  editedItem: {},
+  pdf:{},
+});
+
+export const actions ={
+  async getSupplies({commit}, {companyId}){
+    const service =this.$getRiceService(SupplyService);
+    try{
+      const supplies = await service.getPaginatedSupplies({ companyId });
+      commit('storeSupplies', supplies.data.data);
+      commit('storePagination', supplies.data);
+    }catch(error){
+      commit('catchError', error);
+    }
+    commit('changeLoading', false)
+  },
+
+  async getAllSupplies({commit}, {companyId}){
+    const service =this.$getRiceService(SupplyService);
+    try{
+      const supplies = await service.getAllSupplies({ companyId });
+      commit('storeAllSupplies', supplies.data);
+    }catch(error){
+      commit('catchError', error);
+    }
+  },
+
+  async registerSupply({commit}, {supply}){
+    const service = this.$getRiceService(SupplyService);
+    try{
+      await service.registerSupply({supply});
+    }catch(error){
+      commit('catchError', error);
+    }
+  },
+
+  async editSupply({commit}, {supply}){
+    const service = this.$getRiceService(SupplyService);
+    try{
+      await service.editSupply({supply});
+    }catch(error){
+      commit('catchError', error);
+    }
+  },
+  async deleteSupply({commit}, {supplyId}) {
+    const service =this.$getRiceService(SupplyService);
+    try{
+      await service.deleteSupply({ supplyId });
+    }catch(error){
+      commit('catchError', error);
+    }
+  },
+  async getSupplyMetricTypes({commit}){
+    const service =this.$getRiceService(SupplyService);
+    try{
+      const metricTypes = await service.getSupplyMetricTypes();
+      commit('storeMetricTypes', metricTypes.data);
+    }catch(error){
+      commit('catchError', error);
+    }
+  },
+  async exportSupplies({commit}, {communityId, search}){
+    const service =this.$getRiceService(SupplyService);
+    try{
+      const pdf = await service.exportSupplies({communityId, search});
+      const dataPDF = new Blob([pdf.data],{
+        type:'text/xml'
+      });
+      pdf.data = dataPDF;
+      return pdf;
+    }catch(error){
+      commit('catchError', error);
+    }
+  },
+}
+
+export const getters ={
+  fromId: _state => id => _state.supplies[id],
+};
+
+export const mutations= {
+  storeSupplies(_state, supplies) {
+    _state.supplies = supplies;
+  },
+  storeAllSupplies(_state, supplies) {
+    _state.allSupplies = supplies;
+  },
+  changeLoading(_state, loading) {
+    _state.loading = loading;
+  },
+  catchError(_state, error) {
+    _state.error = error;
+  },
+  storePagination(_state, page){
+    _state.page = page;
+  },
+  storeMetricTypes(_state, metricTypes){
+    _state.metricTypes = metricTypes;
+  },
+  storePDF(_state, pdf){
+    _state.pdf = pdf;
+  }
+}
